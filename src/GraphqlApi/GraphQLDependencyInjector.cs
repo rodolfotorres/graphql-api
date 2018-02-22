d83a0edc5d8683804734000b8108806e846368f3
Ryan Versaw <ryan@versaw.com>
2018-02-22 11:14:42 -0600

    Refactored the injection a bit

---
+++ src/GraphqlApi/GraphQLDependencyInjector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using GraphQL.Conventions;
+
+namespace GraphQLApi
+{
+    public class GraphQLDependencyInjector : IDependencyInjector
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public GraphQLDependencyInjector(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public object Resolve(TypeInfo typeInfo) => _serviceProvider.GetService(typeInfo.AsType());
+    }
+}
